366170370745ba41e903a89daf744573e5b37bd0
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2021-10-24 11:27:18 -0700

    Updated cli

--- object_detection/cli.py
+++ object_detection/cli.py
@@ -1,10 +1,17 @@
 from obj_detection_img_text_out import *
 from obj_detection_img_box_out import *
+import cv2
 
 
 def main():
+
+    img = "z_valet_obj.jpg"
+
     # Get text out with detected objects as a list
-    img_object_detection_txt(...)
+    img_object_detection_txt(img)
 
     # Get image out with detected objects in bounding boxes
-    # img_object_detection_box(...)
+    img_object_detection_box(img)
+
+    # Close all open windows
+    cv2.destroyAllWindows()
