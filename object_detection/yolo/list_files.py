05a1ef07acce075c2b72e2657320a0c171ae9a4b
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2021-10-24 11:27:54 -0700

    Created script to list specific files in a directory and save in .txt

---
+++ object_detection/yolo/list_files.py
@@ -0,0 +1,21 @@
+# TO CREATE train.txt and test.txt for training datasets with YOLO
+
+import glob
+import os
+
+directory_path = r"C:\Users\Nico\darknet\build\darknet\x64\data\obj"
+
+with open("output.txt", "w") as f:
+
+    files = glob.glob(directory_path + '/**/*.jpg', recursive=True)
+
+    for file in files:
+
+        f.write(str(file + "\n"))
+
+    #
+    #     filename = os.path.split(file)[-1]
+    #
+    #     yolo_path = os.path.join("data/obj/" + filename)
+    #
+    #     f.write(str(yolo_path + "\n"))
