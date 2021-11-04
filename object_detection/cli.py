58728f33b2f7fbf3df7fd48c22984a37a51af62f
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2021-11-04 13:17:15 -0700

    Started implementing obj detection of video returning a list

--- object_detection/cli.py
+++ object_detection/cli.py
@@ -1,6 +1,7 @@
 from obj_detection_img_text_out import *
 from obj_detection_img_box_out import *
 # from obj_detection_video_box_out import *
+from obj_detection_video_text_out import *
 
 
 def main():
@@ -10,19 +11,27 @@ def main():
     weights_path = "../object_detection/yolo/weights/yolov4-tiny-obj_170000.weights"
     labels_path = "../object_detection/yolo/obj_names/blackbeard.names"
 
-    img = "z_valet_obj.jpg"
+    # YOLOV3 & COCO for testing function
+    # config_path = "../object_detection/yolo/cfg/yolov3.cfg"
+    # weights_path = "../object_detection/yolo/weights/yolov3.weights"  # wget https://pjreddie.com/media/files/yolov3.weights
+    # labels_path = "../object_detection/yolo/obj_names/coco.names"  # COCO Labels (https://cocodataset.org/)
 
-    # Get text out with detected objects as a list
-    img_object_detection_txt(img, config_path, weights_path, labels_path)
-
-    # Get image out with detected objects in bounding boxes
-    img_object_detection_box(img, config_path, weights_path, labels_path)
+    # img = "z_valet_obj.jpg"
+    #
+    # # Get text out with detected objects as a list
+    # img_object_detection_txt(img, config_path, weights_path, labels_path)
+    #
+    # # Get image out with detected objects in bounding boxes
+    # img_object_detection_box(img, config_path, weights_path, labels_path)
 
     # vid = "busy_intersection.mp4"
-    # vid = "live_blackjack_short_2.mp4"
+    vid = "live_blackjack_short_2.mp4"
 
     # Get video out with detected objects in bounding boxes
     # vid_object_detection_box(vid, config_path, weights_path, labels_path)
+
+    # Get text out with detected objects as a list
+    vid_object_detection_txt(vid, config_path, weights_path, labels_path)
 
     # Close all open windows
     # cv2.destroyAllWindows()
