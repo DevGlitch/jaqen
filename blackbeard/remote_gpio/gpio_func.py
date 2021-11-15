5608ec134b36e40f68eb356b2f35921b65b25cdc
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2021-11-14 23:28:44 -0800

    Implemented remote GPIO functions

--- blackbeard/remote_gpio/gpio_func.py
+++ blackbeard/remote_gpio/gpio_func.py
@@ -1,50 +1,83 @@
-from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Button, LED
 from time import sleep
 import numpy as np
 
 
-def turn_led_on(pin=None, blink=1):
+def connect_remote_gpio(host_ip):
+    """ Connect to remote GPIO host
+    :param host_ip: IP address of your raspberry pi
+    :return: factory object
+    """
+    factory = PiGPIOFactory(host=host_ip)
+    return factory
+
+
+def turn_led_on_off(pin, factory, blink, time_on=0.5, time_off=1):
     """ Turn on and off specific LED
     :param pin: GPIO pin number the LED is connected to
+    :param factory: PiGPIOFactory Host
     :param blink: Number of times the led should blink
+    :param time_on: Seconds the led should stay
+    :param time_off: Seconds the led should stay
     :return: LED Blinking
     """
-    led = LED(pin)
+    led = LED(pin, pin_factory=factory)
 
-    try:
-
-        for i in np.arange(blink):
-            led.on()
-            print(f"LED with pin {pin} turned on.")
-            sleep(1)
-            led.off()
-            print(f"LED with pin {pin} turned off.")
-
-    finally:
-        sleep(1)
+    for i in np.arange(blink):
+        led.on()
+        print(f"LED at pin {pin} turned on.")
+        sleep(time_on)
+        led.off()
+        print(f"LED at pin {pin} turned off.")
+        sleep(time_off)
 
 
-def temp_text_on_pi_display(text="temporary hello", seconds=3):
-    """ Display text temporary on Raspberry Pi
-    :param text: text that should be displayed
-    :param seconds: number of seconds the text should stay displayed
-    :return: Display temporary text
+def button_wait_for_press(pin, factory, timeout=None):
+    """ Wait for button to be pressed
+    :param pin: GPIO pin number the button is connected to
+    :param factory: PiGPIOFactory Host
+    :param timeout: Number of seconds to wait before proceeding (None = Wait indefinitely)
+    :return: Pause script until button is pressed
     """
-    text = ...
+    button = Button(pin, pin_factory=factory)
+    print(f"Waiting for button at pin {pin} to be pressed.")
+    button.wait_for_press(timeout)
+    print(f"The button at pin {pin} was pressed.")
+    return True
 
 
-def perm_text_on_pi_display(text="hello"):
-    """ Display text permanently on Raspberry Pi
-    :param text: text that should be displayed
-    :return: Display permanent text
+def button_wait_for_release(pin, factory, timeout=None):
+    """ Wait for button to be released
+    :param pin: GPIO pin number the button is connected to
+    :param factory: PiGPIOFactory Host
+    :param timeout: Number of seconds to wait before proceeding (None = Wait indefinitely)
+    :return: Pause script until button is released
     """
-    text = ...
+    button = Button(pin, pin_factory=factory)
+    print(f"Waiting for button at pin {pin} to be released.")
+    button.wait_for_press(timeout)
+    print(f"The button at pin {pin} was released.")
+    return True
 
 
-def display_card_count(counter):
-    ...
+def button_when_pressed(pin, factory):
+    """
+    :param pin:
+    :param factory:
+    :return:
+    """
+    button = Button(pin, pin_factory=factory)
+    button.when_pressed()
+    print(f"The button at pin {pin} was pressed.")
 
 
-def display_card_detected(detected_cards):
-    ...
-
+def button_when_released(pin, factory):
+    """
+    :param pin:
+    :param factory:
+    :return:
+    """
+    button = Button(pin, pin_factory=factory)
+    button.when_released()
+    print(f"The button at pin {pin} was released.")
